50c89277d81da24b310414900d1793e18f74d8ae
vbazia <vbazia@wearelearning.com>
2018-09-21 18:37:40 +0200

    nice

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Fight/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Fight from './components/Fight.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Fight
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
 </style>
---
+++ client/src/components/Fight.vue
@@ -0,0 +1,19 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: 'Fight',
+    props: {
+        FightObject: Object
+    },
+    methods: {
+    },
+    mounted() {
+    }
+}
+</script>
+
+<style scoped>
+</style>
